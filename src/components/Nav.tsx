0217e4ba3aa9590572d3a0f779b7ad4c7909586f
Michael Henry Davies <michael_methodtheatre.com>
2025-04-21 17:44:22 -0600

    final application

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,19 @@
+import { Link } from 'react-router-dom'
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <div className="nav">
+      <ul>
+        <li className="nav-item">
+          <Link to="/" className="nav-link">Search Candidates</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/SavedCandidates">Saved Candidates</Link>
+        </li>
+      </ul>
+
+    </div>
   )
 };
 
